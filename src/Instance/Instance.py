4e54247de421dcbf7a1551a5c579c7df2a795113
bengloo <engloo.benjamin@gmail.com>
2023-10-21 10:31:41 +0200

    first commit

---
+++ src/Instance/Instance.py
@@ -0,0 +1,5 @@
+class Instance:
+    def __init__(self,N_OFs_total,contraintes,OFs):
+        self.N_OFs_total=N_OFs_total #nombre d'OF admissible a la journee ciblé
+        self.contraintes=contraintes
+        self.OFs=OFs
