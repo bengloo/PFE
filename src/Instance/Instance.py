afbec6d643f54e6057e061fc39c3886c5ceb983a
bengloo <engloo.benjamin@gmail.com>
2023-10-21 10:36:08 +0200

    up

--- src/Instance/Instance.py
+++ src/Instance/Instance.py
@@ -1,5 +1,6 @@
 class Instance:
-    def __init__(self,N_OFs_total,contraintes,OFs):
+    def __init__(self,N_OFs_total,contraintes,OFs,date):
         self.N_OFs_total=N_OFs_total #nombre d'OF admissible a la journee ciblé
         self.contraintes=contraintes
         self.OFs=OFs
+        self.date=date
